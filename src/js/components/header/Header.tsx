986877aaff45c23a7d3f9c4611b4f01edd421895
Bibiana Bartošová <bibiana.bartosova@mediafactory.cz>
2022-06-07 09:14:17 +0200

    show button just in list

--- src/js/components/header/Header.tsx
+++ src/js/components/header/Header.tsx
@@ -4,21 +4,23 @@ import useAppStateContext from '../../hooks/useAppStateContext';
 import { FormattedMessage } from 'react-intl';
 
 const Header: React.FC = () => {
-  const { reset } = useAppStateContext();
+  const { reset, appState } = useAppStateContext();
 
   return (
     <div className={styles.header}>
       <h1>
         <FormattedMessage defaultMessage="Home Calculator" id="header.title" />
       </h1>
-      <button
-        type="button"
-        onClick={() => {
-          reset();
-        }}
-      >
-        <FormattedMessage defaultMessage="Back" id="header.back" />
-      </button>
+      {'list' === appState.page ? (
+        <button
+          type="button"
+          onClick={() => {
+            reset();
+          }}
+        >
+          <FormattedMessage defaultMessage="Back" id="header.back" />
+        </button>
+      ) : null}
     </div>
   );
 };
